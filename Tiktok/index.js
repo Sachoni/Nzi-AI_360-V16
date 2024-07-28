393b46cce5744e3a4d15d35ffa6abd4b557b4571
Nzi <167908919+Sachoni@users.noreply.github.com>
2024-07-28 17:12:49 +0300

    index.js

--- Tiktok/index.js
+++ Tiktok/index.js
@@ -1,1 +1,16 @@
+const bot = require(__dirname + '/lib/smd')
+const { VERSION } = require(__dirname + '/config')
 
+const start = async () => {
+    Debug.info(`Nzi ${VERSION}`)
+  try {
+    await bot.init()
+    bot.logger.info('⏳ Database syncing!')
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
+}
+start();
